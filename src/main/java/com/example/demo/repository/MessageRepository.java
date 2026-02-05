0a9e0c0f6e880d26ecff80098362e35c4b766c4d
tejaturaka <tejaturaka1@gmail.com>
2026-02-06 00:41:20 +0530

    Fixed repository syntax errors

--- src/main/java/com/example/demo/repository/MessageRepository.java
+++ src/main/java/com/example/demo/repository/MessageRepository.java
@@ -1,3 +1,11 @@
+//package com.example.demo.repository;
+//
+//import com.example.demo.entity.Message;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//@Repository
+//public interface MessageRepository extends JpaRepository<Message, Long> {}
 package com.example.demo.repository;
 
 import com.example.demo.entity.Message;
@@ -5,4 +13,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MessageRepository extends JpaRepository<Message, Long> {}
+public interface MessageRepository extends JpaRepository<Message, Long> {
+}
--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -1,17 +1,26 @@
-// UserRepository.java
+//// UserRepository.java
+//package com.example.demo.repository;
+//import com.example.demo.entity.User;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//@Repository
+//public interface UserRepository extends JpaRepository<User, String> {}
+//
+//// MessageRepository.java
+//package com.example.demo.repository;
+//import com.example.demo.entity.Message;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//@Repository
+//public interface MessageRepository extends JpaRepository<Message, Long> {}
 package com.example.demo.repository;
+
 import com.example.demo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, String> {}
-
-// MessageRepository.java
-package com.example.demo.repository;
-import com.example.demo.entity.Message;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MessageRepository extends JpaRepository<Message, Long> {}
+public interface UserRepository extends JpaRepository<User, String> {
+}
